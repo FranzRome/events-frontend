050d3eb5e253e708b07cb1bba79577b56a8185f4
Franz <francesco.romeo-work@libero.it>
2024-08-11 00:30:43 +0200

    Login Implementation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import './Login.css'
+import React, { FormEvent, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import Button from "../Button/Button";
+import TextField from "../TextField/TextField";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      {}
-    </div>
-  );
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+   const [error, setError] = useState("");
+   const navigate = useNavigate();
+
+   const handleLogin = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+      e.preventDefault();
+      try {
+         const response = await axios.post("http://localhost:4000/auth/login", {
+            username,
+            password,
+         });
+         localStorage.setItem("token", response.data.access_token);
+         navigate("/events"); // Update with success route
+      } catch (error: any) {
+         // Error handling
+         const errorMessage =
+            error.response?.data?.message || "Login failed. Please try again.";
+         setError(errorMessage);
+      }
+   };
+
+   return (
+      <div className="login-container">
+         <h1>Login</h1>
+         <form onSubmit={handleLogin}>
+            <div className="username">
+               <label>Username:</label>
+               <TextField
+                  type="text"
+                  value={username}
+                  id="username"
+                  onChange={e => setUsername(e.target.value)}
+               />
+            </div>
+            <div className="password">
+               <label>Password:</label>
+               <TextField
+                  type="password"
+                  value={password}
+                  id="username"
+                  onChange={e => setPassword(e.target.value)}
+               />
+            </div>
+            <div className="btn-login">
+               <Button
+                       text="Login"
+                       backgroundColor="#007bff"
+                       onClick={() => handleLogin}
+                     />
+            </div>
+         </form>
+         {error && <p style={{ color: "red" }}>{error}</p>}
+         <p>
+            Non hai un account? <Link to="/signup">Iscriviti</Link>
+         </p>
+      </div>
+   );
 };
 
 export default Login;
