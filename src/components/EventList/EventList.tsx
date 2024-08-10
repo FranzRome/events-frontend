e17ff0c0efe7966c1d0ea831f5c0eefae1f48229
Franz <francesco.romeo-work@libero.it>
2024-08-11 00:23:41 +0200

    Event List Restyle & Auth Check

--- src/components/EventList/EventList.tsx
+++ src/components/EventList/EventList.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { getEvents, Event } from '../../services/eventService';
+import Button from '../Button/Button';
+import Login from '../Login/Login';
 import './EventList.css';
 
 const EventList = () => {
    const [events, setEvents] = useState<Event[]>([]);
+   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+   const navigate = useNavigate();
 
    useEffect(() => {
-      fetchEvents();
-   }, []);
+      const token = localStorage.getItem('token');
+      if (token) {
+         setIsAuthenticated(true);
+         fetchEvents();
+      } else {
+         navigate('/login');
+      }
+   }, [navigate]);
 
    const fetchEvents = async () => {
       try {
@@ -19,34 +29,65 @@ const EventList = () => {
       }
    };
 
+   const handleLogout = () => {
+      localStorage.removeItem('token');
+      setIsAuthenticated(false);
+      navigate('/login');
+   };
+
    return (
       <div>
-         <div className="event-list">
-            {
-            events.length === 0 ?
-            <p>Caricamento...</p> :
-            events.map(event => (
-               <div key={event._id} className="event-card">
-                  <h2>{event.name}</h2>
-                  <p>{event.description}</p>
-                  <p>
-                     <strong>Data:</strong>{''}
-                     {new Date(event.date).toLocaleDateString('it-IT', {
-                        year: 'numeric',
-                        month: 'numeric',
-                        day: 'numeric',
-                     })}
-                  </p>
-                  <Link to={`/events/${event._id}`} className="btn-details">
-                     Dettagli
-                  </Link>
+         {isAuthenticated ? (
+            <div>
+               <h1>
+                 Lista Eventi
+               </h1>
+               <div className="event-list">
+                  {events.length === 0 ? (
+                     <p>Caricamento...</p>
+                  ) : (
+                     events.map((event) => (
+                        <div key={event._id} className="event-card" onClick={() => navigate(`/events/${event._id}`)}>
+                           <h2 className='name'>{event.name}</h2>
+                           <p className='description'>{event.description}</p>
+                           <p className='date'>
+                              <strong>Data:</strong>{' '}
+                              {new Date(event.date).toLocaleDateString('it-IT', {
+                                 year: 'numeric',
+                                 month: 'numeric',
+                                 day: 'numeric',
+                              })}
+                           </p>
+                           <Link to={`/events/${event._id}`} className="btn-details">
+                              Dettagli
+                           </Link>
+                        </div>
+                     ))
+                  )}
                </div>
-            ))}
-         </div>
-         <div className='button-container'>
-               <Link to='/event-form' className='btn-add-event'>Aggiungi Evento</Link>
-               <Link to="/signup" className="btn-signup">Registrati</Link>
-         </div>
+               <div className="button-container">
+                  <Button
+                    text="Aggiungi Evento"
+                    backgroundColor="#28a745"
+                    onClick={() => navigate('/event-form')}
+                  />
+                  {/*
+                  <Button
+                    text="Registrati"
+                    backgroundColor="#007bff"
+                    onClick={() => navigate('/signup')}
+                  />
+                  */}
+                  <Button
+                    text="Logout"
+                    backgroundColor="#dc3545"
+                    onClick={handleLogout}
+                  />
+               </div>
+            </div>
+         ) : (
+            <Login />
+         )}
       </div>
    );
 };
