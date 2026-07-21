4776c605ebf9456856260d72e1068ea511dd2478
Francesco Romeo <francesco.romeo-work@libero.it>
2026-07-21 14:17:35 +0200

    Removed Comment

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element: Component, ...rest }: any) => {
-   const isAuthenticated = localStorage.getItem("token"); // Assicurati che questo controllo sull'autenticazione sia adatto alle tue esigenze
+   const isAuthenticated = localStorage.getItem("token");
 
    return isAuthenticated ? Component : <Navigate to="/login" replace />;
 };
